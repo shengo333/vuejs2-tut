d1c6f760aa7d0eb9c749bb4d1bd1f791b8f3e8fe
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-14 11:15:49 +0400

    props in nutshel

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Header />
-    <Ninjas />
+    <Ninjas :ninjas="bekas" />
     <Footer />
     
   </div>
@@ -22,7 +22,14 @@ export default {
     },
   data(){
     return{
-
+        bekas: [
+          {name: 'Ryu', speciality: 'Vue Components', show: false},
+          {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+          {name: 'Hitoshi', speciality: 'Click Events', show: false},
+          {name: 'Tango', speciality: 'Conditionals', show: false},
+          {name: 'Kami', speciality: 'Webpack', show: false},
+          {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+        ]
     }
   }
 }
