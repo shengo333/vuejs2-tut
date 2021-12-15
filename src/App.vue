f51613b795ee47574a8934af116ebc643758ca6f
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-15 11:43:45 +0400

    some more binding

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,19 @@
 <template>
-  
-  <formHelper>
-    <template v-slot:title> <h3> {{title}}</h3> </template>
-    
-    <template v-slot:form-header>
-      <h3>This is the title of the form </h3>
-      <p>information about the slot</p>
-    </template>
-
-    <template v-slot:form-fields>
-      <input type="text" placeholder="name" required>
-      <input type="password" placeholder="password" required>
-    </template>
-
-    
-    <template v-slot:form-controls>
-      <button @click="handleSubmit">Submit</button>
-    </template>
-
-  </formHelper>
+  <addBlog />
 </template>
 
 <script>
 
-import formHelper from './components/formHelper.vue'
-
-
+import addBlog from './components/addBlog.vue'
 
 export default {
     components:{
-         formHelper,
+      addBlog
         
     },
   data(){
     return{
-      title: 'i am dynamic slot title'
+      
     }
   },
   methods:{
@@ -45,8 +24,9 @@ export default {
 
 <style scoped>
 
-h3{
-  color: red
+body{
+  margin: 0;
+  font-family: 'Nunito Semibold';
 }
 
 </style>
